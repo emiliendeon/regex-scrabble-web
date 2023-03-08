9f30aa1b4455b38ac7d9b7d864ac432d808a4b41
Émilien Déon <emilien.deon@gmail.com>
2023-03-08 19:51:36 +0100

    ci: deploy workflow

--- src/store.ts
+++ src/store.ts
@@ -16,7 +16,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
 	key: "root",
 	storage,
-	whitelist: ["dictionary"],
+	whitelist: [],
 };
 
 const rootReducer = persistCombineReducers(persistConfig, {
